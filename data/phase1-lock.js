d52cc6ea611b41eaab7bbfa7b1399a2c17dac495
finance <kfcccpro@gmail.com>
2026-09-20 01:19:58 +0900

    Mark textbook phase 1 data as locked

---
+++ data/phase1-lock.js
@@ -0,0 +1,7 @@
+DATA.meta.phase1Status="PHASE1_LOCKED";
+DATA.meta.phase1ScopePrintedPages=[11,52];
+DATA.meta.referenceTermRows=192;
+DATA.meta.semanticSenseRows=192;
+DATA.meta.referenceUniqueCharacters=187;
+DATA.meta.sourceVisualAudit="printed-pages-11-52-complete";
+DATA.meta.phase1LockedAt="2026-09-20";
